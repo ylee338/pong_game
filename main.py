2cd19926130db793645201013a07c2f82ebb38fa
Young <ylee1359256@gmail.com>
2023-04-21 01:27:34 -0400

    created paddle class and corresponding key bindings

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from paddle import Paddle
+import time
+
 
 screen = Screen()
+screen.bgcolor("black")
+screen.title("Pong Game")
 screen.setup(700, 700)
+screen.tracer(0)
+paddle = Paddle()
+
+screen.listen()
+screen.onkey(fun=paddle.right_up, key='Up')
+screen.onkey(fun=paddle.right_down, key='Down')
+screen.onkey(fun=paddle.left_up, key='w')
+screen.onkey(fun=paddle.left_down, key='s')
+
+
+
+is_playing = True
+
+while is_playing:
+    screen.update()
+    time.sleep(0.1)
+
+
+
+
 
 
 
