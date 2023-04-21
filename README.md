d134207eebdd61d7777f2ce63c34ecfbca69766a
Young <ylee1359256@gmail.com>
2023-04-20 23:48:15 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Classic Pong Game
+
+The classic pong game everyone grew up playing with.
---
+++ main.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(700, 700)
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
